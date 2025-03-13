44da77d80380987675cf57cae6a270c7b082d177
Jorge Rios <jorge.rios@REM-MC-240829B.local>
2025-03-12 19:16:46 -0600

    ya funciona cuenta vamos a pagos

--- servidor.py
+++ servidor.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, request
 import os
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
+#import sqlalchemy
 from sqlalchemy import create_engine
 import os
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+#from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy import text
 from flask_cors import CORS
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 connection = None
 #jaja
@@ -25,9 +28,11 @@ app = Flask(__name__)
 if ENV == "production":
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://arcadia:pinares2024@postgres-db.clm8ssljcpfm.us-east-1.rds.amazonaws.com:5432/arcadia"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "super-secret"
 else:    
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jorge.rios:Mexiquito1991$@localhost/arcadia"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "super-secret"
 #engine2 = create_engine('postgresql://iclarpro:2015@localhost/arcadia', connect_args={'options': '-csearch_path={}'.format('public,arcadia,public')})
 if ENV == "production":
     engine = create_engine("postgresql://arcadia:pinares2024@postgres-db.clm8ssljcpfm.us-east-1.rds.amazonaws.com:5432/arcadia")
@@ -39,6 +44,7 @@ else:
 db = SQLAlchemy(app)
 
 CORS(app)
+jwt = JWTManager(app)
 
 
 class Cliente(db.Model):
@@ -89,16 +95,75 @@ class Cuenta(db.Model):
     fk_tipo_cuenta = db.Column(db.Integer, db.ForeignKey("tipo_cuenta.id"), nullable=True)
     congelada = db.Column(db.Integer, nullable=True)  # Consider changing to Boolean if appropriate
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+class Inmueble(db.Model):
+    __tablename__ = 'inmueble'  # Specifies the table name
 
+    # Define columns as per your PostgreSQL schema
+    codigo = db.Column(db.Integer, primary_key=True)  # Primary key
+    iden1 = db.Column(db.String, nullable=True)
+    iden2 = db.Column(db.String, nullable=True)
+    tipo = db.Column(db.String, nullable=True)
+    superficie = db.Column(db.Numeric, nullable=True)
+    titulo1 = db.Column(db.String, nullable=True)
+    lindero1 = db.Column(db.String, nullable=True)
+    titulo2 = db.Column(db.String, nullable=True)
+    lindero2 = db.Column(db.String, nullable=True)
+    titulo3 = db.Column(db.String, nullable=True)
+    lindero3 = db.Column(db.String, nullable=True)
+    titulo4 = db.Column(db.String, nullable=True)
+    lindero4 = db.Column(db.String, nullable=True)
+    indiviso = db.Column(db.Numeric, nullable=True)
+    condominio = db.Column(db.String, nullable=True)
+    cuentacatastral = db.Column(db.String, nullable=True)
+    precio = db.Column(db.Numeric, nullable=True)
+    inmueble = db.Column(db.String, nullable=False)  # This column is NOT NULL
+    superficiecasa = db.Column(db.Numeric, nullable=True)
+    domiciliooficial = db.Column(db.String, nullable=True)
+    fechadeventa = db.Column(db.Date, nullable=True)
+    fk_etapa = db.Column(db.Integer, nullable=False)  # Foreign key to etapa
+    escriturado = db.Column(db.String, nullable=True)
+    fk_escrituras = db.Column(db.Integer, nullable=True)
+    fk_notario = db.Column(db.Integer, nullable=True)
+    fechaescriturado = db.Column(db.String, nullable=True)
+    preciopormetro = db.Column(db.Numeric, nullable=True)
+    referenciapago = db.Column(db.String, nullable=True)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    username = request.json.get("usuario", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 @app.route('/api/clientes', methods=['GET'])
+@jwt_required()
 def get_clientes():
-    result = db.session.execute(text("SELECT distinct(c.codigo), c.nombre, c.rfc, cc.saldo, i.iden1, i.iden2, c.codigo  FROM cuenta cc join cliente c on c.codigo=cc.fk_cliente join inmueble i on cc.fk_inmueble=i.codigo where saldo>0"))
+    result = db.session.execute(text("""SELECT distinct(cc.codigo) as cuenta, c.codigo as cliente, 
+            c.nombre, c.rfc, cc.saldo, i.iden1, i.iden2 
+            FROM cuenta cc join cliente c on c.codigo=cc.fk_cliente 
+            join inmueble i on cc.fk_inmueble=i.codigo where saldo>0"""))
     clientes = result.fetchall()
 
     # Convert list of tuples to a list of dictionaries
-    clientes_list = [{"id":cliente[0], "cuenta": cliente[0], "nombre": cliente[1], "rfc": cliente[2], "saldo":cliente[3], "manzana":cliente[4], "lote":cliente[5], "cliente":cliente[6]} for cliente in clientes]
+    clientes_list = [{"id":cliente[1], "cuenta": cliente[0], 
+            "nombre": cliente[2], "rfc": cliente[3], "saldo":cliente[4], 
+            "manzana":cliente[5], "lote":cliente[6]} for cliente in clientes]
     response  =jsonify(clientes_list)
     # response.headers.add("Access-Control-Allow-Origin", "*")  # Allow all origins
     # response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
@@ -106,15 +171,13 @@ def get_clientes():
     return response
 
 @app.route('/api/clientes/<int:id>', methods=['GET'])
+@jwt_required()
 def get_cliente(id):
     user = Cliente.query.get(id)
     if not user:
         return jsonify({"error": "Cliente not found"}), 404
 
     response  =jsonify(user.as_dict())
-    # response.headers.add("Access-Control-Allow-Origin", "*")  # Allow all origins
-    # response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-    # response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
     return response
 
 @app.route('/api/clientes', methods=['POST'])
@@ -137,6 +200,25 @@ def add_cliente():
 
     #return jsonify({"id": new_id, "name": new_cliente['name'], "email": new_cliente['email']}), 201
     return jsonify({})
+
+
+@app.route('/api/cuenta/<int:id>', methods=['GET'])
+@jwt_required()
+def cuenta_id(id):
+    cuenta = Cuenta.query.get(id)
+    if not cuenta:
+        return jsonify({"error": "Cuenta not found"}), 404
+    response  =jsonify(cuenta.as_dict())
+    return response
+
+@app.route('/api/inmueble/<int:id>', methods=['GET'])
+@jwt_required()
+def inmueble_id(id):
+    inmueble = Inmueble.query.get(id)
+    if not inmueble:
+        return jsonify({"error": "inmueble not found"}), 404
+    response  =jsonify(inmueble.as_dict())
+    return response
 
 
 if __name__ == '__main__':
